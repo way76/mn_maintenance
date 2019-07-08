76bdfb68e1ea62a81242a07b3b935e64eed6f207
way76 <43696467+way76@users.noreply.github.com>
2019-07-08 15:37:13 +0200

    Create marc_status.sh

---
+++ marc_status.sh
@@ -0,0 +1,66 @@
+mn1b=$(sudo -Hu marcoin-mn1 marcoin-cli getinfo | grep blocks)
+mn1s=$(sudo -Hu marcoin-mn1 marcoin-cli getmasternodestatus | grep message)
+mn2b=$(sudo -Hu marcoin-mn2 marcoin-cli getinfo | grep blocks)
+mn2s=$(sudo -Hu marcoin-mn2 marcoin-cli getmasternodestatus | grep message)
+mn3b=$(sudo -Hu marcoin-mn3 marcoin-cli getinfo | grep blocks)
+mn3s=$(sudo -Hu marcoin-mn3 marcoin-cli getmasternodestatus | grep message)
+mn4b=$(sudo -Hu marcoin-mn4 marcoin-cli getinfo | grep blocks)
+mn4s=$(sudo -Hu marcoin-mn4 marcoin-cli getmasternodestatus | grep message)
+mn5b=$(sudo -Hu marcoin-mn5 marcoin-cli getinfo | grep blocks)
+mn5s=$(sudo -Hu marcoin-mn5 marcoin-cli getmasternodestatus | grep message)
+mn6b=$(sudo -Hu marcoin-mn6 marcoin-cli getinfo | grep blocks)
+mn6s=$(sudo -Hu marcoin-mn6 marcoin-cli getmasternodestatus | grep message)
+mn7b=$(sudo -Hu marcoin-mn7 marcoin-cli getinfo | grep blocks)
+mn7s=$(sudo -Hu marcoin-mn7 marcoin-cli getmasternodestatus | grep message)
+mn8b=$(sudo -Hu marcoin-mn8 marcoin-cli getinfo | grep blocks)
+mn8s=$(sudo -Hu marcoin-mn8 marcoin-cli getmasternodestatus | grep message)
+mn9b=$(sudo -Hu marcoin-mn9 marcoin-cli getinfo | grep blocks)
+mn9s=$(sudo -Hu marcoin-mn9 marcoin-cli getmasternodestatus | grep message)
+mn10b=$(sudo -Hu marcoin-mn10 marcoin-cli getinfo | grep blocks)
+mn10s=$(sudo -Hu marcoin-mn10 marcoin-cli getmasternodestatus | grep message)
+mn11b=$(sudo -Hu marcoin-mn11 marcoin-cli getinfo | grep blocks)
+mn11s=$(sudo -Hu marcoin-mn11 marcoin-cli getmasternodestatus | grep message)
+mn12b=$(sudo -Hu marcoin-mn12 marcoin-cli getinfo | grep blocks)
+mn12s=$(sudo -Hu marcoin-mn12 marcoin-cli getmasternodestatus | grep message)
+mn13b=$(sudo -Hu marcoin-mn13 marcoin-cli getinfo | grep blocks)
+mn13s=$(sudo -Hu marcoin-mn13 marcoin-cli getmasternodestatus | grep message)
+mn14b=$(sudo -Hu marcoin-mn14 marcoin-cli getinfo | grep blocks)
+mn14s=$(sudo -Hu marcoin-mn14 marcoin-cli getmasternodestatus | grep message)
+mn15b=$(sudo -Hu marcoin-mn15 marcoin-cli getinfo | grep blocks)
+mn15s=$(sudo -Hu marcoin-mn15 marcoin-cli getmasternodestatus | grep message)
+mn16b=$(sudo -Hu marcoin-mn16 marcoin-cli getinfo | grep blocks)
+mn16s=$(sudo -Hu marcoin-mn16 marcoin-cli getmasternodestatus | grep message)
+mn17b=$(sudo -Hu marcoin-mn17 marcoin-cli getinfo | grep blocks)
+mn17s=$(sudo -Hu marcoin-mn17 marcoin-cli getmasternodestatus | grep message)
+mn18b=$(sudo -Hu marcoin-mn18 marcoin-cli getinfo | grep blocks)
+mn18s=$(sudo -Hu marcoin-mn18 marcoin-cli getmasternodestatus | grep message)
+mn19b=$(sudo -Hu marcoin-mn19 marcoin-cli getinfo | grep blocks)
+mn19s=$(sudo -Hu marcoin-mn19 marcoin-cli getmasternodestatus | grep message)
+mn20b=$(sudo -Hu marcoin-mn20 marcoin-cli getinfo | grep blocks)
+mn20s=$(sudo -Hu marcoin-mn20 marcoin-cli getmasternodestatus | grep message)
+
+
+
+
+echo "Masternode 1:  $mn1b - $mn1s "
+echo "Masternode 2:  $mn2b - $mn2s "
+echo "Masternode 3:  $mn3b - $mn3s "
+echo "Masternode 4:  $mn4b - $mn4s "
+echo "Masternode 5:  $mn5b - $mn5s "
+echo "Masternode 6:  $mn6b - $mn6s "
+echo "Masternode 7:  $mn7b - $mn7s "
+echo "Masternode 8:  $mn8b - $mn8s "
+echo "Masternode 9:  $mn9b - $mn9s "
+echo "Masternode 10: $mn10b - $mn10s "
+echo "Masternode 11: $mn11b - $mn11s "
+echo "Masternode 12: $mn12b - $mn12s "
+echo "Masternode 13: $mn13b - $mn13s "
+echo "Masternode 14: $mn14b - $mn14s "
+echo "Masternode 15: $mn15b - $mn15s "
+echo "Masternode 16: $mn16b - $mn16s "
+echo "Masternode 17: $mn17b - $mn17s "
+echo "Masternode 18: $mn18b - $mn18s "
+echo "Masternode 19: $mn19b - $mn19s "
+echo "Masternode 20: $mn20b - $mn20s "
+
+
