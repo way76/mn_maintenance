07f40c3da98c59da3c7b55d7e410380c017b073d
way76 <43696467+way76@users.noreply.github.com>
2019-08-04 19:22:53 +0200

    Update stone_block_verify.sh

--- stone_block_verify.sh
+++ stone_block_verify.sh
@@ -4,9 +4,9 @@ BLOCCO=365000
 EXPBLOCK=$(curl -s4 "http://explorer.stonecoin.rocks/api/getblockcount")
 EXPBLOCKLOW=$(expr $EXPBLOCK - 50)
 EXPBLOCKHIGH=$(expr $EXPBLOCK + 50)
-BLOCKHASH=$(cd /usr/local/bin &&./stone-cli getblockhash $BLOCCO)
+BLOCKHASH=$(/usr/local/bin/stone-cli -datadir=/home/stone-mn1/.stone -conf=/home/stone-mn1/.stone/stone.conf  getblockhash $BLOCCO)
 
-MNBLOCK01=$(cd /usr/local/bin &&./stone-cli getblockcount)
+MNBLOCK01=$(/usr/local/bin/stone-cli -datadir=/home/stone-mn1/.stone -conf=/home/stone-mn1/.stone/stone.conf  getblockcount)
 
 echo "blockhash identificato: $BLOCKHASH"
 
@@ -15,13 +15,14 @@ ver_MN01()
 	if [ "$MNBLOCK01" -ge "$EXPBLOCKLOW" ] && [ "$MNBLOCK01" -le "$EXPBLOCKHIGH" ]; then
 	  echo "$(date +%F_%T) Masternode 01 OK " >> stone_block_manager.log
 	else
-	  echo "$(date +%F_%T) Masternode 01 in aggiornamento $MNBLOCK01 / $EXPBLOCK " >> stone_block_manager.log
-	  cd /usr/local/bin &&./stone-cli invalidateblock $BLOCKHASH
-	  systemctl stop Stone.service
+	  echo "$(date +%F_%T) Masternode in aggiornamento $MNBLOCK01 / $EXPBLOCK " >> stone_block_manager.log
+	  /usr/local/bin/stone-cli -datadir=/home/stone-mn1/.stone -conf=/home/stone-mn1/.stone/stone.conf invalidateblock $BLOCKHASH
+	  systemctl stop stone-mn1.service
+	  /usr/local/bin/stone-cli -datadir=/home/stone-mn1/.stone -conf=/home/stone-mn1/.stone/stone.conf  stop
     sleep 10
-    systemctl start Stone.service
+    systemctl start stone-mn1.service
     sleep 30
-    cd /usr/local/bin &&./stone-cli reconsiderblock $BLOCKHASH
+    /usr/local/bin/stone-cli -datadir=/home/stone-mn1/.stone -conf=/home/stone-mn1/.stone/stone.conf reconsiderblock $BLOCKHASH
 	fi
 }
 
