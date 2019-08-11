f6d204d6bc60f66f6a3e26916f20882a76bd50f2
way76 <43696467+way76@users.noreply.github.com>
2019-08-11 18:01:11 +0200

    Update 1x2_installer.sh

--- 1x2_installer.sh
+++ 1x2_installer.sh
@@ -4,7 +4,7 @@ TMP_FOLDER=$(mktemp -d)
 
 DAEMON_ARCHIVE=${1:-"https://github.com/1X2coin/1X2coin/releases/download/v1.0.0/1x2coin-1.0.0-x86_64-linux-gnu.tar.gz"}
 ARCHIVE_STRIP=""
-DEFAULT_PORT=9214
+DEFAULT_PORT=9300
 
 NODE_IP=$(curl -4 icanhazip.com)
 
