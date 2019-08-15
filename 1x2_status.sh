245822f291a317a2c3526379d64f3f3baff60e1a
way76 <43696467+way76@users.noreply.github.com>
2019-08-15 09:37:18 +0200

    Create 1x2_status.sh

---
+++ 1x2_status.sh
@@ -0,0 +1,64 @@
+mn1b=$(sudo -Hu 1x2coin-mn1 1x2coin-cli getinfo | grep blocks)
+mn1s=$(sudo -Hu 1x2coin-mn1 1x2coin-cli getmasternodestatus | grep message)
+mn2b=$(sudo -Hu 1x2coin-mn2 1x2coin-cli getinfo | grep blocks)
+mn2s=$(sudo -Hu 1x2coin-mn2 1x2coin-cli getmasternodestatus | grep message)
+mn3b=$(sudo -Hu 1x2coin-mn3 1x2coin-cli getinfo | grep blocks)
+mn3s=$(sudo -Hu 1x2coin-mn3 1x2coin-cli getmasternodestatus | grep message)
+mn4b=$(sudo -Hu 1x2coin-mn4 1x2coin-cli getinfo | grep blocks)
+mn4s=$(sudo -Hu 1x2coin-mn4 1x2coin-cli getmasternodestatus | grep message)
+mn5b=$(sudo -Hu 1x2coin-mn5 1x2coin-cli getinfo | grep blocks)
+mn5s=$(sudo -Hu 1x2coin-mn5 1x2coin-cli getmasternodestatus | grep message)
+mn6b=$(sudo -Hu 1x2coin-mn6 1x2coin-cli getinfo | grep blocks)
+mn6s=$(sudo -Hu 1x2coin-mn6 1x2coin-cli getmasternodestatus | grep message)
+mn7b=$(sudo -Hu 1x2coin-mn7 1x2coin-cli getinfo | grep blocks)
+mn7s=$(sudo -Hu 1x2coin-mn7 1x2coin-cli getmasternodestatus | grep message)
+mn8b=$(sudo -Hu 1x2coin-mn8 1x2coin-cli getinfo | grep blocks)
+mn8s=$(sudo -Hu 1x2coin-mn8 1x2coin-cli getmasternodestatus | grep message)
+mn9b=$(sudo -Hu 1x2coin-mn9 1x2coin-cli getinfo | grep blocks)
+mn9s=$(sudo -Hu 1x2coin-mn9 1x2coin-cli getmasternodestatus | grep message)
+mn10b=$(sudo -Hu 1x2coin-mn10 1x2coin-cli getinfo | grep blocks)
+mn10s=$(sudo -Hu 1x2coin-mn10 1x2coin-cli getmasternodestatus | grep message)
+mn11b=$(sudo -Hu 1x2coin-mn11 1x2coin-cli getinfo | grep blocks)
+mn11s=$(sudo -Hu 1x2coin-mn11 1x2coin-cli getmasternodestatus | grep message)
+mn12b=$(sudo -Hu 1x2coin-mn12 1x2coin-cli getinfo | grep blocks)
+mn12s=$(sudo -Hu 1x2coin-mn12 1x2coin-cli getmasternodestatus | grep message)
+mn13b=$(sudo -Hu 1x2coin-mn13 1x2coin-cli getinfo | grep blocks)
+mn13s=$(sudo -Hu 1x2coin-mn13 1x2coin-cli getmasternodestatus | grep message)
+mn14b=$(sudo -Hu 1x2coin-mn14 1x2coin-cli getinfo | grep blocks)
+mn14s=$(sudo -Hu 1x2coin-mn14 1x2coin-cli getmasternodestatus | grep message)
+mn15b=$(sudo -Hu 1x2coin-mn15 1x2coin-cli getinfo | grep blocks)
+mn15s=$(sudo -Hu 1x2coin-mn15 1x2coin-cli getmasternodestatus | grep message)
+mn16b=$(sudo -Hu 1x2coin-mn16 1x2coin-cli getinfo | grep blocks)
+mn16s=$(sudo -Hu 1x2coin-mn16 1x2coin-cli getmasternodestatus | grep message)
+mn17b=$(sudo -Hu 1x2coin-mn17 1x2coin-cli getinfo | grep blocks)
+mn17s=$(sudo -Hu 1x2coin-mn17 1x2coin-cli getmasternodestatus | grep message)
+mn18b=$(sudo -Hu 1x2coin-mn18 1x2coin-cli getinfo | grep blocks)
+mn18s=$(sudo -Hu 1x2coin-mn18 1x2coin-cli getmasternodestatus | grep message)
+mn19b=$(sudo -Hu 1x2coin-mn19 1x2coin-cli getinfo | grep blocks)
+mn19s=$(sudo -Hu 1x2coin-mn19 1x2coin-cli getmasternodestatus | grep message)
+mn20b=$(sudo -Hu 1x2coin-mn20 1x2coin-cli getinfo | grep blocks)
+mn20s=$(sudo -Hu 1x2coin-mn20 1x2coin-cli getmasternodestatus | grep message)
+
+
+
+
+echo "Masternode 1:  $mn1b - $mn1s "
+echo "Masternode 2:  $mn2b - $mn2s "
+echo "Masternode 3:  $mn3b - $mn3s "
+echo "Masternode 4:  $mn4b - $mn4s "
+echo "Masternode 5:  $mn5b - $mn5s "
+echo "Masternode 6:  $mn6b - $mn6s "
+echo "Masternode 7:  $mn7b - $mn7s "
+echo "Masternode 8:  $mn8b - $mn8s "
+echo "Masternode 9:  $mn9b - $mn9s "
+echo "Masternode 10: $mn10b - $mn10s "
+echo "Masternode 11: $mn11b - $mn11s "
+echo "Masternode 12: $mn12b - $mn12s "
+echo "Masternode 13: $mn13b - $mn13s "
+echo "Masternode 14: $mn14b - $mn14s "
+echo "Masternode 15: $mn15b - $mn15s "
+echo "Masternode 16: $mn16b - $mn16s "
+echo "Masternode 17: $mn17b - $mn17s "
+echo "Masternode 18: $mn18b - $mn18s "
+echo "Masternode 19: $mn19b - $mn19s "
+echo "Masternode 20: $mn20b - $mn20s "
