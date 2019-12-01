bebc4bba94675dfa46b2339841db77f25e5018ad
way76 <43696467+way76@users.noreply.github.com>
2019-12-01 12:39:11 +0100

    Create snd_stop.sh

---
+++ snd_stop.sh
@@ -0,0 +1,22 @@
+systemctl stop snodecoin-mn1.service
+systemctl stop snodecoin-mn2.service
+systemctl stop snodecoin-mn3.service
+systemctl stop snodecoin-mn4.service
+systemctl stop snodecoin-mn5.service
+systemctl stop snodecoin-mn6.service
+systemctl stop snodecoin-mn7.service
+systemctl stop snodecoin-mn8.service
+systemctl stop snodecoin-mn9.service
+systemctl stop snodecoin-mn10.service
+systemctl stop snodecoin-mn11.service
+systemctl stop snodecoin-mn12.service
+systemctl stop snodecoin-mn13.service
+systemctl stop snodecoin-mn14.service
+systemctl stop snodecoin-mn15.service
+systemctl stop snodecoin-mn16.service
+systemctl stop snodecoin-mn17.service
+systemctl stop snodecoin-mn18.service
+systemctl stop snodecoin-mn19.service
+systemctl stop snodecoin-mn20.service
+
+killall snodecoind
