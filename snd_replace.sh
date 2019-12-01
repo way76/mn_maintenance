1666331e5f9d0f3fe3b4f51c477d300569d19f5f
way76 <43696467+way76@users.noreply.github.com>
2019-12-01 12:17:06 +0100

    Update snd_replace.sh

--- snd_replace.sh
+++ snd_replace.sh
@@ -1,7 +1,7 @@
-echo -e "Inserisci masternode di origine:"
-read -e ORIGINE
-echo -e "Inserisci masternode di destinazione:"
-read -e DESTINAZIONE
+#!/bin/bash
 
-echo snodecoin-mn${ORIGINE}
+read -p "MN origine:" mn_origine
+read -p "MN destinazione:" mn_dest
+
+echo snodecoin-mn${mn_origine}
 
