9f7080d0197f5eef5245bbb2290d3f4c7a2efd9f
way76 <43696467+way76@users.noreply.github.com>
2020-01-05 12:47:27 +0100

    Update pac_status.sh

--- pac_status.sh
+++ pac_status.sh
@@ -1,4 +1,2 @@
 /usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf getinfo
 /usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf masternode status
-/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn2/.pacglobal -conf=/home/pacglobal-mn2/.pacglobal/pacglobal.conf getinfo
-/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn2/.pacglobal -conf=/home/pacglobal-mn2/.pacglobal/pacglobal.conf masternode status
