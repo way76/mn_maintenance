87b1369547151674f5e4664cb6fa0f849823944d
way76 <43696467+way76@users.noreply.github.com>
2020-01-13 08:12:33 +0100

    Update snd_addnode.sh

--- snd_addnode.sh
+++ snd_addnode.sh
@@ -1,160 +1,29 @@
 addnodes() {
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 104.230.71.248:56943 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 104.40.215.96:50530 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.203.114.140:39646 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:51594 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:51844 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:51861 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:52104 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:52899 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54199 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54228 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54262 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54288 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54298 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54357 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54794 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:54882 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.10:55189 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:55775 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:56921 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:58016 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:58245 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:58550 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:58628 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:58715 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:60344 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:60681 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:62946 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64234 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64335 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64340 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64376 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64384 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64498 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:64544 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.11:65424 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:57764 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:58357 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:59909 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:60262 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:64224 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 109.233.208.9:64225 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 116.202.19.18:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 116.203.102.118:59662 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 116.203.97.194:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 138.201.4.31:55456 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 140.82.39.211:42052 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 149.56.143.91:38686 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 157.97.106.161:51680 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 164.68.108.171:38316 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 164.68.111.175:59506 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.101.236:49464 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.101.236:49760 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.106.56:58414 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.110.168:55696 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.127.216:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.71.237:55106 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.71.237:58210 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.75.120:37340 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.75.120:41678 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.75.120:49410 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.90.85:46732 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.93.186:36604 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 167.86.97.17:44924 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 170.10.16.18:46056 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.212.242.66:55404 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.0.217:47306 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.0.217:47490 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.0.217:49734 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.11.155:48754 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.11.155:50070 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.17.108:41250 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.17.108:41276 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.55.110:51758 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 173.249.7.249:54826 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 178.238.225.107:38044 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 178.238.225.107:38652 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 178.238.225.107:39666 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 178.238.225.107:50416 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 199.247.9.117:48572 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:38040 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:44626 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:44910 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:45054 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:45084 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:45148 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.203.181:46178 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 207.180.240.43:42048 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 213.136.69.28:34872 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 213.136.69.28:47598 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 213.136.77.245:63344 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 46.163.196.103:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 46.172.234.42:42794 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 5.189.148.246:36882 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 5.189.148.246:52198 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 5.77.36.116:59434 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 54.36.198.231:34664 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 66.42.112.6:39736 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 71.19.148.96:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 71.59.6.228:59161 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 72.66.5.155:61502 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 77.55.213.214:47258 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 77.55.222.63:58000 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 77.76.136.171:42200 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 78.141.206.21:41298 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 78.47.95.185:36986 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 79.143.189.132:43948 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.138.176:58756 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.141.88:40300 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.16.130:53768 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.194.26:35310 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.194.26:35348 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.194.26:35924 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.28.30:50902 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.28.30:51000 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 80.211.92.41:49214 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 85.214.55.128:47544 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 85.25.110.22:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 85.25.95.149:18988 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 87.249.206.238:50972 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 87.249.206.238:52466 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 87.249.206.238:52468 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 87.249.206.238:53961 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 88.198.135.168:33912 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 88.99.13.48:40620 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 93.41.3.47:53973 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 94.177.204.85:60018 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.106.110:40468 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.228.183:34426 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.255.182:59884 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.255.182:60006 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.255.234:46226 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.216.38.85:37156 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.217.52.125:33772 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.217.67.204:52252 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.217.67.250:51998 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.217.68.7:42328 add
-sudo -Hu snodecoin-mn$1 snodecoin-cli addnode 95.217.97.222:57284 add
+sudo -Hu snodecoin-mn$1 snodecoin-cli addnode $2 add
+sudo -Hu snodecoin-mn$1 snodecoin-cli addnode $2 add
+
 }
 
-addnodes "1"
-addnodes "2"
-addnodes "3"
-addnodes "4"
-addnodes "5"
-addnodes "6"
-addnodes "7"
-addnodes "8"
-addnodes "9"
-addnodes "10"
-addnodes "11"
-addnodes "12"
-addnodes "13"
-addnodes "14"
-addnodes "15"
-addnodes "16"
-addnodes "17"
-addnodes "18"
-addnodes "19"
-addnodes "20"
+ip1="188.32.113.218:18988"
+ip2="[2001:19f0:6c01:1b97:5400:1ff:0:6]:18988"
+
+addnodes "1", $ip1
+addnodes "2", $ip1
+addnodes "3", $ip1
+addnodes "4", $ip1
+addnodes "5", $ip1
+addnodes "6", $ip1
+addnodes "7", $ip1
+addnodes "8", $ip1
+addnodes "9", $ip1
+addnodes "10", $ip1
+addnodes "11", $ip1
+addnodes "12", $ip1
+addnodes "13", $ip1
+addnodes "14", $ip1
+addnodes "15", $ip1
+addnodes "16", $ip1
+addnodes "17", $ip1
+addnodes "18", $ip1
+addnodes "19", $ip1
+addnodes "20", $ip1
