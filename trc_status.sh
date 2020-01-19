e48c5f2845566281b80cd5bbc11dc9597048f8a2
way76 <43696467+way76@users.noreply.github.com>
2020-01-19 18:55:36 +0100

    Create trc_status.sh

---
+++ trc_status.sh
@@ -0,0 +1,14 @@
+
+sudo -Hu terracoin-mn1 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn1/.terracoincore -conf=/home/terracoin-mn1/.terracoincore/terracoin.conf getinfo
+sudo -Hu terracoin-mn1 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn1/.terracoincore -conf=/home/terracoin-mn1/.terracoincore/terracoin.conf masternode status
+
+sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf getinfo
+sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf masternode status
+
+sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf getinfo
+sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf masternode status
+
+sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf getinfo
+sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf masternode status
+
+
