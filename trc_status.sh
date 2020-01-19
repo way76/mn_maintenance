1c2a7b9f669be80ffec24a5fdcaef33c91410825
way76 <43696467+way76@users.noreply.github.com>
2020-01-19 19:14:06 +0100

    Update trc_status.sh

--- trc_status.sh
+++ trc_status.sh
@@ -1,16 +1,12 @@
-
-sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf getinfo
-sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf masternode status
-
-sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf getinfo
-sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf masternode status
-
-sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf getinfo
-sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf masternode status
-
-
 mn1b=$(sudo -Hu terracoin-mn1 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn1/.terracoincore -conf=/home/terracoin-mn1/.terracoincore/terracoin.conf getinfo | grep blocks)
 mn1s=$(sudo -Hu terracoin-mn1 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn1/.terracoincore -conf=/home/terracoin-mn1/.terracoincore/terracoin.conf masternode status | grep status)
+mn2b=$(sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf getinfo | grep blocks)
+mn2s=$(sudo -Hu terracoin-mn2 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn2/.terracoincore -conf=/home/terracoin-mn2/.terracoincore/terracoin.conf masternode status | grep status)
+mn3b=$(sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf getinfo | grep blocks)
+mn3s=$(sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf masternode status | grep status)
+mn4b=$(sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf getinfo | grep blocks)
+mn4s=$(sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf masternode status | grep status)
+
 
 echo "Masternode 1:  $mn1b - $mn1s "
 echo "Masternode 2:  $mn2b - $mn2s "
