4b21251ae60007e5ab4b50cdb6933c4bffb2f930
way76 <43696467+way76@users.noreply.github.com>
2020-01-23 18:06:13 +0100

    Update pac_status.sh

--- pac_status.sh
+++ pac_status.sh
@@ -1,15 +1,12 @@
-/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf getinfo
-/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf masternode status
 
-
-mn1b=$(sudo -Hu pacglobal-mn1 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn1/.terracoincore -conf=/home/pacglobal-mn1/.terracoincore/terracoin.conf getinfo | grep blocks)
-mn1s=$(sudo -Hu pacglobal-mn1 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn1/.terracoincore -conf=/home/pacglobal-mn1/.terracoincore/terracoin.conf masternode status | grep status)
-mn2b=$(sudo -Hu pacglobal-mn2 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn2/.terracoincore -conf=/home/pacglobal-mn2/.terracoincore/terracoin.conf getinfo | grep blocks)
-mn2s=$(sudo -Hu pacglobal-mn2 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn2/.terracoincore -conf=/home/pacglobal-mn2/.terracoincore/terracoin.conf masternode status | grep status)
-mn3b=$(sudo -Hu pacglobal-mn3 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn3/.terracoincore -conf=/home/pacglobal-mn3/.terracoincore/terracoin.conf getinfo | grep blocks)
-mn3s=$(sudo -Hu pacglobal-mn3 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn3/.terracoincore -conf=/home/pacglobal-mn3/.terracoincore/terracoin.conf masternode status | grep status)
-mn4b=$(sudo -Hu pacglobal-mn4 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn4/.terracoincore -conf=/home/pacglobal-mn4/.terracoincore/terracoin.conf getinfo | grep blocks)
-mn4s=$(sudo -Hu pacglobal-mn4 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn4/.terracoincore -conf=/home/pacglobal-mn4/.terracoincore/terracoin.conf masternode status | grep status)
+mn1b=$(sudo -Hu pacglobal-mn1 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf getinfo | grep blocks)
+mn1s=$(sudo -Hu pacglobal-mn1 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf masternode status | grep status)
+mn2b=$(sudo -Hu pacglobal-mn2 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn2/.pacglobal -conf=/home/pacglobal-mn2/.pacglobal/pacglobal.conf getinfo | grep blocks)
+mn2s=$(sudo -Hu pacglobal-mn2 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn2/.pacglobal -conf=/home/pacglobal-mn2/.pacglobal/pacglobal.conf masternode status | grep status)
+mn3b=$(sudo -Hu pacglobal-mn3 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn3/.pacglobal -conf=/home/pacglobal-mn3/.pacglobal/pacglobal.conf getinfo | grep blocks)
+mn3s=$(sudo -Hu pacglobal-mn3 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn3/.pacglobal -conf=/home/pacglobal-mn3/.pacglobal/pacglobal.conf masternode status | grep status)
+mn4b=$(sudo -Hu pacglobal-mn4 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn4/.pacglobal -conf=/home/pacglobal-mn4/.pacglobal/pacglobal.conf getinfo | grep blocks)
+mn4s=$(sudo -Hu pacglobal-mn4 /usr/local/bin/pacglobal-cli  -datadir=/home/pacglobal-mn4/.pacglobal -conf=/home/pacglobal-mn4/.pacglobal/pacglobal.conf masternode status | grep status)
 
 
 echo "Masternode 1:  $mn1b - $mn1s "
