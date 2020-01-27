552f152360b8f71d48088df3dc6f6bf14549e0b3
way76 <43696467+way76@users.noreply.github.com>
2020-01-27 13:45:44 +0100

    Update snd_addnode.sh

--- snd_addnode.sh
+++ snd_addnode.sh
@@ -23,12 +23,5 @@ sudo -Hu snodecoin-mn19 snodecoin-cli addnode $1 add
 sudo -Hu snodecoin-mn20 snodecoin-cli addnode $1 add
 }
 
-addnodes "[2001:19f0:6c01:1b97:5400:1ff:0:4]:18988"
-addnodes "[2001:19f0:6c01:1b97:5400:1ff:0:6]:18988"
-addnodes "[2605:2700:1:1002::5]:18988"
-addnodes "24.183.7.119:18988"
-addnodes "[2a02:2168:a04:59f0::1]:18988"
-addnodes "167.86.127.216:18988"
-addnodes "77.76.136.170:18988"
-addnodes "94.177.172.227:18988"
+addnodes "173.212.247.119:18988"
 
