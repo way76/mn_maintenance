3858d0abec5ed2723bafb7787c071f987c4657ca
way76 <43696467+way76@users.noreply.github.com>
2020-02-08 11:55:49 +0100

    Update snode_installer18.sh

--- snode_installer18.sh
+++ snode_installer18.sh
@@ -26,7 +26,7 @@ NC='\033[0m'
 function checks() 
 {
   if [[ $(lsb_release -d) != *16.04* ]]; then
-    #echo -e " ${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+    echo -e " ${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
     #exit 1
   fi
 
