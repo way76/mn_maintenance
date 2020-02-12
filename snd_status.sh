ebae99b424a1f4fa8b6a11f66d79b4c026a38e89
way76 <43696467+way76@users.noreply.github.com>
2020-02-12 08:32:53 +0100

    Update snd_status.sh

--- snd_status.sh
+++ snd_status.sh
@@ -44,7 +44,7 @@ calcola_max() {
 mnb=$1
 max=$2
 
-echo "Ricevuto $mnb $max"
+#echo "Ricevuto $mnb $max"
 
 mnb=${$mnb%,}
 mnb=${$mnb##*:}
@@ -56,7 +56,7 @@ echo "$max"
 
 
 max=0
-echo "versione 2"
+echo "versione 3"
 echo "Valore m1 rilevato: " $(calcola_max "$mn1b" "$max")
 echo "Valore m2 rilevato: " $(calcola_max "$mn2b" "$max")
 echo "Valore m3 rilevato: " $(calcola_max "$mn3b" "$max")
