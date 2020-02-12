a9ae53060901c9db02acbe171a61e8095e6c0c44
way76 <43696467+way76@users.noreply.github.com>
2020-02-12 12:04:42 +0100

    Update snd_status.sh

--- snd_status.sh
+++ snd_status.sh
@@ -55,7 +55,7 @@ mn1b=${mnb##*:}
 mn1b=${mn1b%,} 
 
  if [[ "$mn1b" -gt "$min" ]]; then 
-   echo -e "Masternode $pos: ${RED}$mnb - $mns ${NC}" 
+   echo -e "Masternode $pos: $mnb - $mns - ATTENZIONE" 
  else
    echo -e "Masternode $pos: $mnb - $mns" 
     fi
