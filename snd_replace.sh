f2f24976191dae39fe3b830925db7deecadd4503
way76 <43696467+way76@users.noreply.github.com>
2020-02-16 18:38:56 +0100

    Update snd_replace.sh

--- snd_replace.sh
+++ snd_replace.sh
@@ -3,7 +3,34 @@
 
 ripr_mn()
 {
- echo "MN da rimuovere: $1, con origine $2"
+mnr=$1
+mno=$2
+
+systemctl stop snodecoin-mn$mnr.service
+rm /home/snodecoin-mn$mnr/.snodecoin/blocks -r
+rm /home/snodecoin-mn$mnr/.snodecoin/chainstate -r
+rm /home/snodecoin-mn$mnr/.snodecoin/sporks -r
+rm /home/snodecoin-mn$mnr/.snodecoin/zerocoin -r
+rm /home/snodecoin-mn$mnr/.snodecoin/backups -r
+rm /home/snodecoin-mn$mnr/.snodecoin/database -r
+rm /home/snodecoin-mn$mnr/.snodecoin/banlist.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/db.log
+rm /home/snodecoin-mn$mnr/.snodecoin/fee_estimates.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/governance.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/instantsend.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/mempool.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/mncache.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/netfulfilled.dat 
+rm /home/snodecoin-mn$mnr/.snodecoin/peers.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/sporks.dat
+cd /home/snodecoin-mn$mnr/.snodecoin/
+rm debug*.*
+#systemctl start snodecoin-mn4.service
+#cd /root
+#sleep 30
+#./snd_addnodes.sh
+#./snd_status.sh
+ 
 
 }
  
