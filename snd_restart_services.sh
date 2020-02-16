6ee94bd183cd935590a48ffbc3f71c81114b662f
way76 <43696467+way76@users.noreply.github.com>
2020-02-16 04:31:11 +0100

    Update snd_restart_services.sh

--- snd_restart_services.sh
+++ snd_restart_services.sh
@@ -1,3 +1,10 @@
+attesa()
+{
+sleep 300
+
+}
+
+
 echo "Fermo i servizi:"
 echo "1:"
 systemctl stop snodecoin-mn1.service
@@ -46,62 +53,62 @@ sleep 60
 echo "Avvio nuovamente i servizi:"
 echo "1:"
 systemctl start snodecoin-mn1.service
-sleep 1200
+attesa
 echo "2:"
 systemctl start snodecoin-mn2.service
-sleep 1200
+attesa
 echo "3:"
 systemctl start snodecoin-mn3.service
-sleep 1200
+attesa
 echo "4:"
 systemctl start snodecoin-mn4.service
-sleep 1200
+attesa
 echo "5:"
 systemctl start snodecoin-mn5.service
-sleep 1200
+attesa
 echo "6:"
 systemctl start snodecoin-mn6.service
-sleep 1200
+attesa
 echo "7:"
 systemctl start snodecoin-mn7.service
-sleep 1200
+attesa
 echo "8:"
 systemctl start snodecoin-mn8.service
-sleep 1200
+attesa
 echo "9:"
 systemctl start snodecoin-mn9.service
-sleep 1200
+attesa
 echo "10:"
 systemctl start snodecoin-mn10.service
-sleep 1200
+attesa
 echo "11:"
 systemctl start snodecoin-mn11.service
-sleep 1200
+attesa
 echo "12:"
 systemctl start snodecoin-mn12.service
-sleep 1200
+attesa
 echo "13:"
 systemctl start snodecoin-mn13.service
-sleep 1200
+attesa
 echo "14:"
 systemctl start snodecoin-mn14.service
-sleep 1200
+attesa
 echo "15:"
 systemctl start snodecoin-mn15.service
-sleep 1200
+attesa
 echo "16:"
 systemctl start snodecoin-mn16.service
-sleep 1200
+attesa
 echo "17:"
 systemctl start snodecoin-mn17.service
-sleep 1200
+attesa
 echo "18:"
 systemctl start snodecoin-mn18.service
-sleep 1200
+attesa
 echo "19:"
 systemctl start snodecoin-mn19.service
-sleep 1200
+attesa
 echo "20:"
 systemctl start snodecoin-mn20.service
-sleep 1200
+attesa
 echo "Tutti i servizi sono stati avviati"
