917174cc85ddca435520274a4b072c922c4c0d0d
way76 <43696467+way76@users.noreply.github.com>
2020-02-16 16:33:22 +0100

    Update snd_replace.sh

--- snd_replace.sh
+++ snd_replace.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+
+
+ripr_mn()
+{
+ echo "MN da rimuovere: $mnr, con oigine $mno"
+
+}
  
-echo "Hi, please type the word: "
-read  word
-echo "The word you entered is: $word"
-echo "Can you please enter two words? "
-read word1 word2
-echo "Here is your input: \"$word1\" \"$word2\""
-echo "How do you feel about bash scripting? "
-# read command now stores a reply into the default build-in variable $REPLY
-read
-echo "You said $REPLY, I'm glad to hear that! "
-echo -e "What are your favorite colours ? "
-# -a makes read command to read into an array
-read -a colours
-echo "My favorite colours are also ${colours[0]}, ${colours[1]} and ${colours[2]}:-)" 
+echo "Masternode da rimuovere:"
+read  mnr
+echo "Masternode di origine:"
+read  mno
+
+ripr_mn "$mnr" "$mno"
