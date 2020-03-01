6933e54884e2ef8edab242868a28327fb3dc34c4
way76 <43696467+way76@users.noreply.github.com>
2020-03-01 18:33:34 +0100

    Update masternodes.sh

--- masternodes.sh
+++ masternodes.sh
@@ -21,6 +21,7 @@ esac
 }
 menu_snd()
 {
+echo "  SNODECOIn "
 echo "  1) status masternodes  "
 echo "  2) restart masternodes "
 echo "  3) copia masternode    "
@@ -36,8 +37,26 @@ case $snd_azione in
   *) menu_snd;;
 esac
 }
+menu_marc()
+{
+echo "  MARCOIN "
+echo "  1) status masternodes  "
+echo "  2) restart masternodes "
+echo "  3) copia masternode    "
+echo "  9) torna indietro      "
 
 
+read marc_azione
+case $marc_azione in
+  1) marc_status;menu_marc;;
+  2) marc_restart;menu_marc;;
+  3) marc_copia;menu_marc;;
+  9) menu_coin;;
+  *) menu_marc;;
+esac
+}
+
+############################################# SNODECOIN
 snd_status()
 {
  wget -qO - https://raw.githubusercontent.com/way76/mn_maintenance/master/snd_status.sh > /root/mn_scripts/snd_status.sh
@@ -62,6 +81,29 @@ snd_copia()
  chmod +755 /root/mn_scripts/snd_replace.sh
  chmod +x /root/mn_scripts/snd_replace.sh
  /root/mn_scripts/snd_replace.sh
+}
+
+####################################################### MARCOIN
+marc_status()
+{
+ wget -qO - https://raw.githubusercontent.com/way76/mn_maintenance/master/marc_status.sh > /root/mn_scripts/marc_status.sh
+ chmod +755 /root/mn_scripts/marc_status.sh
+ chmod +x /root/mn_scripts/marc_status.sh
+ /root/mn_scripts/marc_status.sh
+}
+marc_restart()
+{
+ wget -qO - https://raw.githubusercontent.com/way76/mn_maintenance/master/marc_restart_services.sh > /root/mn_scripts/marc_restart_services.sh
+ chmod +755 /root/mn_scripts/marc_restart_services.sh
+ chmod +x /root/mn_scripts/marc_restart_services.sh
+ /root/mn_scripts/marc_restart_services.sh
+}
+marc_copia()
+{
+ wget -qO - https://raw.githubusercontent.com/way76/mn_maintenance/master/marc_replace.sh > /root/mn_scripts/marc_replace.sh
+ chmod +755 /root/mn_scripts/marc_replace.sh
+ chmod +x /root/mn_scripts/marc_replace.sh
+ /root/mn_scripts/marc_replace.sh
 }
 
 
