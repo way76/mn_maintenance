2685c45fb9ef88a71aa759015ab39c825017b083
way76 <43696467+way76@users.noreply.github.com>
2020-03-01 12:07:19 +0100

    Update masternodes.sh

--- masternodes.sh
+++ masternodes.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
 
+function update_user 
+{
+ option=0
+ until [ "$option" = "3"]; do
+ echo "  1.) Update username"
+ echo "  2.) Update password"
+ echo "  3.) Return to menu"
 
-#####
-#
-# the menu
-#
-#####
-
-#mainmenu subroutine
-
-function mainmenu {
-
-select menusel in "perform backup" "HS server administration" "DU server administration" "Multi server administration" "Shell administration" "" "Logout" "command line" "HELP!" "EXIT PROGRAM"; do
-case $menusel in
-	"perform backup")
-		echo "you chose option 1" ;;
-	
-	"HS server administration")
-		echo "you chose option 2" ;;
-	
-	"DU server administration")
-		echo "you chose option 3" ;;
-	
-	"Multi server administration")
-		echo "you chose option 4" ;;
-	
-	"Shell administration")
-		echo "you chose option 5" ;;
-	
-	"Logout")
-		echo "you chose option 8" ;;
-	
-	"command line")
-		echo "you chose option 9" ;;
-	
-	"HELP!")
-		echo "i wish!" ;;
-
-	"EXIT PROGRAM")
-		exit 0 ;;
-esac
-
-break
-
-done
+ echo -n "Enter choice: "
+ read option
+ echo ""
+ case $option in
+ 1 ) update_username; press_enter ;;
+ 2 ) update_password; press_enter ;;
+ 3 ) main_menu; press_enter ;;
+ 4 ) break ;;
+ * ) tput setf 3;echo "Please enter 1, 2 or 3";tput setf 3; 
+ esac
+#   }
+ done
 }
 
-while true; do mainmenu; done
+function main_menu 
+{
+option=0
+until [ "$option" = "4" ]; do
+echo "  1.) Add user"
+echo "  2.) Remove user"
+echo "  3.) Update user"
+echo "  4.) Quit"
+
+echo -n "Enter choice: "
+read option
+echo ""
+case $option in
+    1 ) add_user ; press_enter ;;
+    2 ) remove_user ; press_enter ;;
+    3 ) update_user ; press_enter ;;
+    4 ) exit;;
+    * ) tput setf 4;echo "Please enter 1, 2, 3, or 4";tput setf 4; 
+esac
+done
+ }
+
+main_menu
