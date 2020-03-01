b38e269ccb210ad4137e2e02ae706ec40abe0f33
way76 <43696467+way76@users.noreply.github.com>
2020-03-01 12:24:50 +0100

    Update masternodes.sh

--- masternodes.sh
+++ masternodes.sh
@@ -1,29 +1,47 @@
-submenu() {
-    while true; do
-        select submenu1 in 'Submenu 1' 'Submenu 2' 'Submenu 3' 'Exit to main menu'
-        do
-            case $REPLY in
-                1) echo 'SubMenu1' ;;
-                2) echo 'SubMenu2' ;;
-                3) echo 'SubMenu3' ;;
-                4) break 2 ;;
-                *) echo 'Please select an option.' >&2
-            esac
-            break
-        done
-    done
-}
+#!/bin/bash
+while true
+do
+# Parent menu items declared here
+select item in Mouse Printer Monitor HDD
+do
 
-while true; do
-    select mainMenu in 'Main Menu 1' 'Main Menu 2' 'Main Menu 3' 'Quit'
-    do
-        case $REPLY in
-            1) submenu ;;
-            2) echo 'MainMenu2' ;;
-            3) echo 'MainMenu3' ;;
-            4) break 2 ;;
-            *) echo 'Please select an option.' >&2
-        esac
-        break
-    done
+# case statement to compare the first menu items
+case $item in
+Mouse)
+echo "Which type of monitor you like?"
+;;
+Printer)
+# Sub-menu items here declared here
+select sub_item in Samsung HP Dell
+do
+# case statement for sun-menu items
+case $sub_item in
+Samsung)
+echo "The price of samsung printer is $100"
+break
+;;
+HP)
+echo "New HP 880 printer price printer is $450"
+# return to parent menu
+break
+;;
+Dell)
+echo "No Dell printer is available now"
+# return to the parent menu
+break 2
+;;
+esac
+done
+break
+;;
+Monitor)
+echo "Buy our new Samsung Monitor at low cost"
+break # return to current (main) menu
+;;
+HDD)
+echo "Good quality HDD are available in the stock"
+# return from the script
+break 2
+esac
+done
 done
