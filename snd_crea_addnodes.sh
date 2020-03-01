26f4243764fd53c909b662930df5bd4b36e0ac62
way76 <43696467+way76@users.noreply.github.com>
2020-03-01 19:08:43 +0100

    Update snd_crea_addnodes.sh

--- snd_crea_addnodes.sh
+++ snd_crea_addnodes.sh
@@ -1,29 +1,29 @@
 addnodes() {
-sudo -Hu snodecoin-mn1 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn2 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn3 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn4 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn5 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn6 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn7 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn8 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn9 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn10 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn11 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn12 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn13 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn14 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn15 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn16 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn17 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn18 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn19 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
-sudo -Hu snodecoin-mn20 snodecoin-cli getpeerinfo | grep  addr -w >> /root/snd_nodes.txt
+sudo -Hu snodecoin-mn1 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn2 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn3 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn4 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn5 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn6 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn7 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn8 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn9 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn10 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn11 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn12 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn13 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn14 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn15 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn16 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn17 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn18 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn19 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
+sudo -Hu snodecoin-mn20 snodecoin-cli getpeerinfo | grep  addr -w >> /root/mn_scripts/snd_nodes.txt
 }
 
-cd /root
-rm /root/snd_nodes1.txt
-rm /root/snd_nodes.txt
+cd /root/mn_scripts/
+rm /root/mn_scripts/snd_nodes1.txt
+rm /root/mn_scripts/snd_nodes.txt
 addnodes
 sed -e 's/\s\+//g' snd_nodes.txt > snd_nodes1.txt
 sed -e 's/\"addr\":\+/addnode=/g' snd_nodes1.txt > snd_nodes.txt
