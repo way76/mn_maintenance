8714d4aa12db0e9f257cc0e6fae15c34be8f2afb
way76 <43696467+way76@users.noreply.github.com>
2020-05-09 10:36:16 +0200

    Update trc_status.sh

--- trc_status.sh
+++ trc_status.sh
@@ -6,9 +6,12 @@ mn3b=$(sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terra
 mn3s=$(sudo -Hu terracoin-mn3 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn3/.terracoincore -conf=/home/terracoin-mn3/.terracoincore/terracoin.conf masternode status | grep status)
 mn4b=$(sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf getinfo | grep blocks)
 mn4s=$(sudo -Hu terracoin-mn4 /usr/local/bin/terracoin-cli  -datadir=/home/terracoin-mn4/.terracoincore -conf=/home/terracoin-mn4/.terracoincore/terracoin.conf masternode status | grep status)
+mn5b=$(sudo -Hu mnt /usr/local/bin/terracoin-cli  -datadir=/home/mnt/.terracoincore -conf=/home/mnt/.terracoincore/terracoin.conf getinfo | grep blocks)
+mn5s=$(sudo -Hu mnt /usr/local/bin/terracoin-cli  -datadir=/home/mnt/.terracoincore -conf=/home/mnt/.terracoincore/terracoin.conf masternode status | grep status)
 
 
 echo "Masternode 1:  $mn1b - $mn1s "
 echo "Masternode 2:  $mn2b - $mn2s "
 echo "Masternode 3:  $mn3b - $mn3s "
 echo "Masternode 4:  $mn4b - $mn4s "
+echo "Masternode 5:  $mn5b - $mn5s "
