dac157c42cddb869e312e1be39a53ce66890dcca
way76 <43696467+way76@users.noreply.github.com>
2020-08-15 11:42:39 +0200

    Update 1x2_addnode.sh

--- 1x2_addnode.sh
+++ 1x2_addnode.sh
@@ -1,40 +1,41 @@
 addnode() {
 echo "aggiungo ip $1 alla configurazione "
 echo " "
-sudo -Hu snodecoin-mn1 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn2 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn3 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn4 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn5 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn6 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn7 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn8 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn9 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn10 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn11 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn12 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn13 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn14 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn15 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn16 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn17 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn18 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn19 snodecoin-cli addnode $1 add
-sudo -Hu snodecoin-mn20 snodecoin-cli addnode $1 add
+sudo -Hu 1x2coin-mn1 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn2 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn3 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn4 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn5 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn6 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn7 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn8 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn9 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn10 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn11 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn12 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn13 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn14 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn15 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn16 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn17 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn18 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn19 1x2coin-cli addnode $1 add
+sudo -Hu 1x2coin-mn20 1x2coin-cli addnode $1 add
 }
-addnode "82.165.23.144:20001"
-addnode "82.165.23.144:20002"
-addnode "82.165.158.250:20001"
-addnode "82.165.158.250:20002"
-addnode "217.160.172.48:20001"
-addnode "217.160.172.48:20002"
-addnode "217.160.142.80:20001"
-addnode "217.160.142.80:20002"
-addnode "212.227.192.172:20001"
-addnode "212.227.192.172:20002"
-addnode "93.90.194.235:20001"
-addnode "93.90.194.235:20002"
-addnode "217.160.246.137:20001"
-addnode "217.160.246.137:20002"
-addnode "217.160.244.245:20001"
-addnode "217.160.244.245:20002"
+addnode "45.77.244.116:9214"
+addnode "85.94.179.41:9214"
+addnode "80.241.208.52:9214"
+addnode "95.217.2.16:9214"
+addnode "46.4.89.67:9214"
+addnode "173.212.240.125:9214"
+addnode "173.249.32.221:9214"
+addnode "164.68.116.141:9214"
+addnode "5.189.128.48:9214"
+addnode "195.201.232.162:9214"
+addnode "167.86.103.161:9214"
+addnode "116.203.118.180:9214"
+addnode "140.82.36.29:9214"
+addnode "148.251.138.3:9214"
+addnode "51.83.71.27:9214"
+addnode "185.147.75.101:9214"
+
