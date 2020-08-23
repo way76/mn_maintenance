72f670fd400fd955e328ed526514fcd06410c5a8
way76 <43696467+way76@users.noreply.github.com>
2020-08-23 09:10:32 +0200

    Update and rename dogec_add_nodes.sh to dogec_addnode.sh

---
+++ dogec_addnode.sh
@@ -0,0 +1,42 @@
+addnode() {
+echo "aggiungo ip $1 alla configurazione "
+echo " "
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn1/.dogecash/dogecash.conf -datadir=/home/doge_mn1/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn2/.dogecash/dogecash.conf -datadir=/home/doge_mn2/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn3/.dogecash/dogecash.conf -datadir=/home/doge_mn3/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn4/.dogecash/dogecash.conf -datadir=/home/doge_mn4/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn5/.dogecash/dogecash.conf -datadir=/home/doge_mn5/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn6/.dogecash/dogecash.conf -datadir=/home/doge_mn6/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn7/.dogecash/dogecash.conf -datadir=/home/doge_mn7/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn8/.dogecash/dogecash.conf -datadir=/home/doge_mn8/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn9/.dogecash/dogecash.conf -datadir=/home/doge_mn9/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn10/.dogecash/dogecash.conf -datadir=/home/doge_mn10/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn11/.dogecash/dogecash.conf -datadir=/home/doge_mn11/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn12/.dogecash/dogecash.conf -datadir=/home/doge_mn12/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn13/.dogecash/dogecash.conf -datadir=/home/doge_mn13/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn14/.dogecash/dogecash.conf -datadir=/home/doge_mn14/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn15/.dogecash/dogecash.conf -datadir=/home/doge_mn15/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn16/.dogecash/dogecash.conf -datadir=/home/doge_mn16/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn17/.dogecash/dogecash.conf -datadir=/home/doge_mn17/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn18/.dogecash/dogecash.conf -datadir=/home/doge_mn18/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn19/.dogecash/dogecash.conf -datadir=/home/doge_mn19/.dogecash/ addnode $1 add
+/usr/local/bin/dogecash-cli -conf=/home/doge_mn20/.dogecash/dogecash.conf -datadir=/home/doge_mn20/.dogecash/ addnode $1 add
+}
+
+addnode "[2a02:c205:2024:7058:750e::24]:56740"
+addnode "167.86.97.16:33244"
+addnode "164.68.102.164:56492"
+addnode "167.86.124.251:43338"
+addnode "23.154.160.182:25738"
+addnode "173.249.36.55:49026"
+addnode "207.180.198.130:35528"
+addnode "167.86.88.172:38338"
+addnode "95.179.191.88:35088"
+addnode "5.135.136.102:58142"
+addnode "164.68.102.162:55014"
+addnode "167.86.94.254:58494"
+addnode "207.180.228.156:45776"
+addnode "167.86.95.12:49038"
+addnode "164.68.102.158:35644"
+addnode "167.86.81.59:56604"
+addnode "167.86.97.7:41892"
