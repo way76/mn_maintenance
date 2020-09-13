1d5076abbce47146841e6c7b8e8666aa8cd5d992
way76 <43696467+way76@users.noreply.github.com>
2020-09-13 08:25:59 +0200

    Update masternodes.sh

--- masternodes.sh
+++ masternodes.sh
@@ -28,6 +28,7 @@ echo "  2) restart masternodes "
 echo "  3) copia masternode    "
 echo "  4) staking status      "
 echo "  5) unlock wallet       "
+echo "  6)   lock wallet       "
 echo "  9) torna indietro      "
 
 read snd_azione
@@ -37,6 +38,7 @@ case $snd_azione in
   3) snd_copia;menu_snd;;
   4) snd_status_staking;menu_snd;;
   5) snd_wallet_unlock;menu_snd;;
+  6) snd_wallet_lock;menu_snd;;
   9) menu_coin;;
   *) menu_snd;;
 esac
