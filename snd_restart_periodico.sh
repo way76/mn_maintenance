5c11826f7427439ac92a611646a39275dd8f23b2
way76 <43696467+way76@users.noreply.github.com>
2020-09-23 17:04:20 +0200

    Update snd_restart_periodico.sh

--- snd_restart_periodico.sh
+++ snd_restart_periodico.sh
@@ -1,1 +1,5 @@
-
+systemctl stop snodecoin-mn1.service
+sleep 10
+systemctl start snodecoin-mn1.service
+sleep 300
+/usr/local/bin/snodecoin-cli -datadir=/home/snodecoin-mn1/.snodecoin -conf=/home/snodecoin-mn1/.snodecoin/snodecoin.conf walletpassphrase Password@99Casuale 9999999999999 true
