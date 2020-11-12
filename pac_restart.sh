3f2a2d5850f686b5c93ca20957319aa62d0ddb85
way76 <43696467+way76@users.noreply.github.com>
2020-11-12 17:26:37 +0100

    Update pac_restart.sh

--- pac_restart.sh
+++ pac_restart.sh
@@ -1,33 +1,7 @@
-attesa()
-{
+
+systemctl restart pacglobal-mn1.service
 sleep 180
-
-}
-
-echo "Fermo i servizi:"
-echo "1:"
-systemctl stop pacglobal-mn1.service
-echo "2:"
-systemctl stop pacglobal-mn2.service
-echo "3:"
-systemctl stop pacglobal-mn3.service
-echo "4:"
-systemctl stop pacglobal-mn4.service
-
-killall terracoind
-echo "Attesa riavvio servizi:"
-sleep 60
-echo "Avvio nuovamente i servizi:"
-echo "1:"
-systemctl start pacglobal-mn1.service
-attesa
-echo "2:"
-systemctl start pacglobal-mn2.service
-attesa
-echo "3:"
-systemctl start pacglobal-mn3.service
-attesa
-echo "4:"
-systemctl start pacglobal-mn4.service
-attesa
-echo "Tutti i servizi sono stati avviati"
+/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf walletpassphrase Password@99Casuale 999999999999
+/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf setstaking true
+sleep 180
+/usr/local/bin/pacglobal-cli -datadir=/home/pacglobal-mn1/.pacglobal -conf=/home/pacglobal-mn1/.pacglobal/pacglobal.conf setstaking true
