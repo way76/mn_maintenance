28042faf7534cc664c9f41c47c4e61db2571632c
way76 <43696467+way76@users.noreply.github.com>
2020-11-15 10:38:12 +0100

    Update dogec_replaceAll.sh

--- dogec_replaceAll.sh
+++ dogec_replaceAll.sh
@@ -2,36 +2,38 @@ ripr_mn()
 {
 mnr=$1
 mno=$2
-echo "Copio il nodo $2 in $1..."
 
-systemctl stop dogec_mn$mnr.service
+systemctl stop doge_mn$mnr.service
+rm /home/doge_mn$mnr/.dogecash/backups -r
+rm /home/doge_mn$mnr/.dogecash/banlist.dat
 rm /home/doge_mn$mnr/.dogecash/blocks -r
-rm /home/doge_mn$mnr/.dogecash/budget.dat
 rm /home/doge_mn$mnr/.dogecash/chainstate -r
-rm /home/doge_mn$mnr/.dogecash/database -r
+rm /home/doge_mn$mnr/.dogecash/budget.dat
 rm /home/doge_mn$mnr/.dogecash/db.log
-rm /home/doge_mn$mnr/.dogecash/debug.log
+rm /home/doge_mn$mnr/.dogecash/debug*
 rm /home/doge_mn$mnr/.dogecash/fee_estimates.dat
-rm /home/doge_mn$mnr/.dogecash/mncache.dat
 rm /home/doge_mn$mnr/.dogecash/mnpayments.dat
 rm /home/doge_mn$mnr/.dogecash/peers.dat
 rm /home/doge_mn$mnr/.dogecash/sporks -r
 rm /home/doge_mn$mnr/.dogecash/zerocoin -r
+cd /home/doge_mn$mnr/.dogecash/
 
 systemctl stop doge_mn$mno.service
 cp -a /home/doge_mn$mno/.dogecash/blocks /home/doge_mn$mnr/.dogecash/blocks
 chown -R doge_mn$mnr:doge_mn$mnr blocks
 cp -a /home/doge_mn$mno/.dogecash/chainstate /home/doge_mn$mnr/.dogecash/chainstate
 chown -R doge_mn$mnr:doge_mn$mnr chainstate
-cp -a /home/doge_mn$mno/.dogecash/database /home/doge_mn$mnr/.dogecash/database
-chown -R doge_mn$mnr:doge_mn$mnr database
 cp -a /home/doge_mn$mno/.dogecash/sporks /home/doge_mn$mnr/.dogecash/sporks
 chown -R doge_mn$mnr:doge_mn$mnr sporks
 cp -a /home/doge_mn$mno/.dogecash/zerocoin /home/doge_mn$mnr/.dogecash/zerocoin
 chown -R doge_mn$mnr:doge_mn$mnr zerocoin
-
+cp /home/doge_mn$mno/.dogecash/peers.dat /home/doge_mn$mnr/.dogecash/peers.dat
+chown -R doge_mn$mnr:doge_mn$mnr peers.dat
+systemctl start doge_mn$mno.service
+sleep 30
 systemctl start doge_mn$mnr.service
-sleep 90
+cd /root
+sleep 30
 
 }
 
