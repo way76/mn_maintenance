9266878a5f4b4117729f68276263f80414e5116f
way76 <43696467+way76@users.noreply.github.com>
2021-02-26 16:43:21 +0100

    Update snd_replace.sh

--- snd_replace.sh
+++ snd_replace.sh
@@ -7,22 +7,17 @@ mnr=$1
 mno=$2
 
 systemctl stop snodecoin-mn$mnr.service
+rm /home/snodecoin-mn$mnr/.snodecoin/backups -r
 rm /home/snodecoin-mn$mnr/.snodecoin/blocks -r
 rm /home/snodecoin-mn$mnr/.snodecoin/chainstate -r
+rm /home/snodecoin-mn$mnr/.snodecoin/db.log
+rm /home/snodecoin-mn$mnr/.snodecoin/debug.log
+rm /home/snodecoin-mn$mnr/.snodecoin/mncache.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/mnpayments.dat
+rm /home/snodecoin-mn$mnr/.snodecoin/peers.dat
 rm /home/snodecoin-mn$mnr/.snodecoin/sporks -r
 rm /home/snodecoin-mn$mnr/.snodecoin/zerocoin -r
-rm /home/snodecoin-mn$mnr/.snodecoin/backups -r
-rm /home/snodecoin-mn$mnr/.snodecoin/database -r
 rm /home/snodecoin-mn$mnr/.snodecoin/banlist.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/db.log
-rm /home/snodecoin-mn$mnr/.snodecoin/fee_estimates.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/governance.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/instantsend.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/mempool.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/mncache.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/netfulfilled.dat 
-rm /home/snodecoin-mn$mnr/.snodecoin/peers.dat
-rm /home/snodecoin-mn$mnr/.snodecoin/sporks.dat
 cd /home/snodecoin-mn$mnr/.snodecoin/
 rm debug*.*
 systemctl stop snodecoin-mn$mno.service
