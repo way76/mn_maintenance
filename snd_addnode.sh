976931220925850fd89e8254854093582e912d15
way76 <43696467+way76@users.noreply.github.com>
2021-03-02 15:51:43 +0100

    Update snd_addnode.sh

--- snd_addnode.sh
+++ snd_addnode.sh
@@ -26,7 +26,10 @@ sudo -Hu snodecoin-mn21 snodecoin-cli addnode $1 add
 
 addnode "94.214.161.52:18988"
 addnode "173.66.179.250:18988"
+addnode "85.93.142.239:18988"
+addnode "108.55.122.40:18988"
 addnode "78.29.27.186:18988"
+addnode "85.94.179.41:18988"
 addnode "173.249.63.116:18988"
 addnode "94.177.236.204:18988"
 addnode "138.201.172.121:18988"
